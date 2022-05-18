cd87930e7bdef5f7cef2e01d586a485321deb84d
Omar <omar.assed@gmail.com>
2022-05-18 09:36:43 -0600

    commit tree b0d031ac6a95a3a8f136ec09909e4d0fffeb79b4

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+const mondayWork = function(mondayActivity) {
+    if (mondayActivity) {
+        return `This Monday, I will ${mondayActivity}`;
+    } else { return "This Monday, I will go to the office"}
+};
